271935189060ec17271c925ac1dd2ed3848858bb
Tushar-OP <tusharpatilmarch@gmail.com>
2020-10-03 23:01:38 +0530

    Changes:  - Explore Page:    1. Centered the text below poster in the expanded box for better look    2. Changes the overall function of fetching of data from API    3. Removed Future Builder and cleaned up the code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
-import 'package:flutter/material.dart';
-import 'package:movie_box/screens/search_page.dart';
-import 'screens/explore_page.dart';
-import 'screens/auth_page.dart';
-import 'screens/welcome_page.dart';
-import 'package:movie_box/constants/styles.dart';
-
-import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
+import 'package:movie_box/constants/styles.dart';
+import 'package:movie_box/screens/search_page.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'models/logged_in_user.dart';
+import 'screens/auth_page.dart';
+import 'screens/explore_page.dart';
+import 'screens/welcome_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,15 +20,16 @@ void main() async {
   runApp(ChangeNotifierProvider(
     create: (context) => LoggedInUser(userId),
     child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          fontFamily: 'SourceSansPro',
-          primaryColor: Colors.white,
-          accentColor: Colors.deepPurple,
-        ),
-        home: WelcomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'SourceSansPro',
+        primaryColor: Colors.white,
+        accentColor: Colors.deepPurple,
       ),
-  ));}
+      home: WelcomePage(),
+    ),
+  ));
+}
 
 class MovieBox extends StatefulWidget {
   @override
@@ -38,7 +38,7 @@ class MovieBox extends StatefulWidget {
 
 class _MovieBoxState extends State<MovieBox> {
   int index = 0;
-  List<Widget> screens =  [
+  List<Widget> screens = [
     ExplorePage(),
     SearchPage(),
     AuthPage(),
@@ -53,28 +53,24 @@ class _MovieBoxState extends State<MovieBox> {
   Widget build(BuildContext context) {
     FlutterStatusbarcolor.setNavigationBarColor(Colors.white, animate: true);
     return Scaffold(
+      //Added to remove renderflex overflow in search while autofocusing on the search field.
+      resizeToAvoidBottomInset: false,
       body: screens[index],
-
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: index,
         elevation: 0.0,
-        unselectedLabelStyle: kBottomNavTextStyle.copyWith(color: Colors.black26),
+        unselectedLabelStyle:
+            kBottomNavTextStyle.copyWith(color: Colors.black26),
         unselectedItemColor: Colors.black54,
-        unselectedIconTheme: IconThemeData(
-            size: 24.0
-        ),
+        unselectedIconTheme: IconThemeData(size: 24.0),
         selectedItemColor: Colors.deepPurple[900],
         selectedLabelStyle: kBottomNavTextStyle,
-        selectedIconTheme: IconThemeData(
-            size: 32.0
-        ),
-
+        selectedIconTheme: IconThemeData(size: 32.0),
         onTap: (i) {
           setState(() {
             index = i;
           });
         },
-
         items: [
           BottomNavigationBarItem(
             icon: Icon(
@@ -88,9 +84,7 @@ class _MovieBoxState extends State<MovieBox> {
             icon: Icon(
               Icons.search,
             ),
-            title: Text(
-              'Search'
-            ),
+            title: Text('Search'),
           ),
           BottomNavigationBarItem(
             icon: Icon(
@@ -105,5 +99,3 @@ class _MovieBoxState extends State<MovieBox> {
     );
   }
 }
-
-
